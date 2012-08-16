706f613320edb7a8c4e0c4cdac2859260c312eee
Luke Bredeson <luke.bredeson@gmail.com>
2012-08-15 19:31:24 -0500

    Maybe the browser will show us a stacktrace now...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Crashfinder::Application.configure do
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
@@ -34,7 +34,11 @@ Crashfinder::Application.configure do
   # config.force_ssl = true
 
   # See everything in the log (default is :info)
-  config.log_level = :debug
+  # config.log_level = :debug
+  
+  # Make sure Heroku can pick up our logs
+  STDOUT.sync = true
+  config.logger = Logger.new(STDOUT) 
 
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
