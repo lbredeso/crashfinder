e569b6e24e8fd5846108d662a506c6911af56e45
Luke Bredeson <luke.bredeson@gmail.com>
2013-07-14 20:29:43 -0500

    Another attempt to fix https Persona login problems

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,12 +2,12 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
-    redirect_to root_url, :notice => "Signed in!"
+    render json: { message: "Signed In!" }
   end
   
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, :notice => "Signed out!"
+    render json: { message: "Signed Out!" }
   end
   
   protected
