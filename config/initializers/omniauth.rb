8cac23e0540a03dd1e6a1ff27b936d4863b7d47e
Luke Bredeson <luke.bredeson@gmail.com>
2013-07-16 00:44:53 -0500

    Fix for https in production for Persona

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :browser_id
 end
+
+if Rails.env.production?
+  OmniAuth.config.full_host = 'https://www.crashfinder.org'
+end
